04f75c4bcb62b44a33ed993a441fb464f3e3ff98
Орест bc2024-5 <orest.kravtsiv@lnu.edu.ua>
2024-11-15 20:01:30 +0200

    Доробив лабораторну

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const { program } = require('commander');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
+const upload = multer();
+
 
 //Налаштував потрібні аргументи
 program
@@ -22,6 +28,94 @@ const host = options.host;
 const port = options.port;
 const cache = options.cache;
 
+// PArt 2
+
+
+
+// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// GET /notes/<ім’я нотатки>
+app.get('/notes/:name', (req, res) => {
+    const noteName = req.params.name;
+    try {
+        const notePath = path.join(cache, `${noteName}.txt`);
+        const noteContent = fs.readFileSync(notePath, 'utf8');
+        res.send(noteContent);
+        console.log(noteName);
+    } catch {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    };
+});
+
+// PUT /notes/<ім’я нотатки>
+app.put('/notes/:name', upload.none(), (req, res) => {
+    const noteName = req.params.name;
+    const { text } = req.body;
+    // console.log(text);
+
+    try {
+        const notePath = path.join(cache, `${noteName}.txt`);
+        fs.writeFileSync(notePath, text, 'utf8');
+        res.status(200).send('Note updated');
+    } catch {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+
+
+});
+
+// DELETE /notes/<ім’я нотатки> 
+app.delete('/notes/:name', (req, res) => {
+    const { noteName } = req.params;
+    try {
+        const notePath = path.join(cache, `${noteName}.txt`);
+        fs.unlinkSync(notePath);
+        res.status(200).send('Note deleted');
+
+    } catch {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+// GET /notes
+app.get('/notes', (req, res) => {
+    const notes = fs.readdirSync(cache)
+        .map(file => {
+            const name = file.slice(0, -4);
+            const notePath = path.join(cache, `${name}.txt`);
+            const text = fs.readFileSync(notePath, 'utf8');
+            return { name, text };
+        });
+    res.status(200).json(notes);
+});
+
+// POST /write
+app.post('/write', upload.none(), (req, res) => {
+    const { note_name, note } = req.body;
+
+    try {
+
+        const notePath = path.join(cache, `${note_name}.txt`);
+        if (fs.existsSync(notePath)) throw error;
+        fs.writeFileSync(notePath, note, 'utf8');
+
+        res.status(201).send('Created');
+
+    }
+    catch {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Bad request');
+    }
+});
+
+app.get('/UploadForm.html', (req, res) => {
+    const filePath = path.join(__dirname, 'UploadForm.html');
+    res.sendFile(filePath);
+});
 
 app.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`);
